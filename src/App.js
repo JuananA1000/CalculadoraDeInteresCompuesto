c331faf8785cde2ba07dfcb7ee08e509cab68dc7
Juan Antonio <juanantonioamillopez@gmail.com>
2022-03-14 11:46:55 +0100

    Creando un contenedor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import styled from 'styled-components';
+
+const Contenedor = styled.div`
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    align-items: center;
+`;
 
 function App() {
-  return (
-    <div>
-      <h1>
-        Calculadora de Interés Compuesto
-      </h1>
-    </div>
-  );
+    return (
+        <Contenedor>
+            <h1>Calculadora de Interés Compuesto</h1>
+        </Contenedor>
+    );
 }
 
 export default App;
