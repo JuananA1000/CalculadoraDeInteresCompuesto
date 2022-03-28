2c44e1ca44a022a5e961e71afda1139880e276ba
Juan Antonio <juanantonioamillopez@gmail.com>
2022-03-28 11:28:22 +0200

    Mostrando el balance

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Formik, Form } from 'formik';
+import { useState } from 'react';
 
 import Input from './components/Input';
 import Boton from './components/Boton';
 import Contenedor from './components/Contenedor';
 import Section from './components/Section';
+import Balance from './components/Balance';
 
 // Fórmula del interés compuesto
 const interesCompuesto = (deposito, contribucion, años, interes) => {
@@ -16,7 +18,25 @@ const interesCompuesto = (deposito, contribucion, años, interes) => {
     return Math.round(total); // Redondeamos el total
 };
 
+// Formato en español y euros
+const formatEU = new Intl.NumberFormat('es-ES', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
+// Formato en dólares americanos
+const formatUSD = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
 function App() {
+    const [balance, setBalance] = useState('');
+
     const handleSubmit = ({ deposito, contribucion, años, interes }) => {
         const resultado = interesCompuesto(
             /*
@@ -28,7 +48,8 @@ function App() {
             Number(años),
             Number(interes)
         );
-        console.log('Resultado Interés Compuesto: ',resultado);
+        console.log('Resultado Interés Compuesto: ', resultado);
+        setBalance(formatEU.format(resultado));
     };
 
     return (
@@ -51,6 +72,10 @@ function App() {
                         <Boton>Calcular</Boton>
                     </Form>
                 </Formik>
+
+                {balance !== '' ? (
+                    <Balance>Balance Final: {balance}</Balance>
+                ) : null}
             </Section>
         </Contenedor>
     );
