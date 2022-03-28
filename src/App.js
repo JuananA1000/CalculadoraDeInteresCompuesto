d5f720c7ca062163c5deea1959ad7791a82d4c71
Juan Antonio <juanantonioamillopez@gmail.com>
2022-03-28 10:31:22 +0200

    Formula de interés compuesto

--- src/App.js
+++ src/App.js
@@ -5,9 +5,30 @@ import Boton from './components/Boton';
 import Contenedor from './components/Contenedor';
 import Section from './components/Section';
 
+// Fórmula del interés compuesto
+const interesCompuesto = (deposito, contribucion, años, interes) => {
+    let total = deposito;
+
+    for (let i = 0; i < años; i++) {
+        total = (total + contribucion) * (interes + 1);
+    }
+
+    return Math.round(total); // Redondeamos el total
+};
+
 function App() {
-    const handleSubmit = () => {
-        // code
+    const handleSubmit = ({ deposito, contribucion, años, interes }) => {
+        const resultado = interesCompuesto(
+            /*
+                Pasamos a número estos datos, porque en initialValues son
+                tipo string
+            */
+            Number(deposito),
+            Number(contribucion),
+            Number(años),
+            Number(interes)
+        );
+        console.log('Resultado Interés Compuesto: ',resultado);
     };
 
     return (
